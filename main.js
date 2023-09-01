4794f15906b68bb8cfcd9961dbf1808bf04d0bf6
Affonso Heinen <affonsomiguel@hotmail.com>
2023-09-01 19:30:06 -0300

    Removido desafio.html e desafio_estilo.css; Retirato do estilo.css codigo referente ao cabecalho; Retirado link para pagina desafio.html; Adicionado o play quado clicado nos botôes, quando clicado neles com Enter e o Espaço; Adicionado condições de verificação se o que está sendo passado como parametro é audio, caso seja nulo ou de outro tipo é mostrado mensagem no console.

--- main.js
+++ main.js
@@ -1,2 +1,31 @@
-document.querySelector('.tecla_pom');
-document.querySelector('input[type=tel]');
+function tocaSom(idElementoAudio) {
+    const elemento = document.querySelector(idElementoAudio);
+
+    if(elemento && elemento.localName === 'audio') {
+        elemento.play();
+    } else {
+        console.log('Elemento não encontrado ou seletor inválido!');
+    }
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for(let contador = 0; contador < listaDeTeclas.length; contador++) {
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function() {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function(evento) {
+        if(evento.code === "Enter" || evento.code === "Space") {
+            tecla.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function() {
+        tecla.remove('ativa');
+    }
+}
